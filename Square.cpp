9a6b0f589f514698401400011bb19b53973f4a02
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-08-25 08:59:54 +0530

    Draw a Square

---
+++ Square.cpp
@@ -0,0 +1,25 @@
+#include<windows.h>
+#include <GL/glut.h>
+using namespace std;
+void displayMe(void)
+{
+glClear(GL_COLOR_BUFFER_BIT);
+glBegin(GL_POLYGON);
+glVertex3f(1,0,0);
+glVertex3f(0,0,0);
+glVertex3f(0,1,0);
+glVertex3f(1,1,0);
+glEnd();
+glFlush();
+}
+int main(int argc, char** argv)
+{
+glutInit(&argc, argv);
+glutInitDisplayMode(GLUT_SINGLE);
+glutInitWindowSize(400, 300);
+glutInitWindowPosition(100, 100);
+glutCreateWindow("Hello world!");
+glutDisplayFunc(displayMe);
+glutMainLoop();
+return 0;
+}
